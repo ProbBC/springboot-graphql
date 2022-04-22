37defae03177e76fa2c8e6d979c3776af4876a91
Gabriel Cruz <gabrielcruz31@gmail.com>
2022-04-21 22:16:31 -0300

    Initial commit

---
+++ src/main/java/com/udemy/compras/graphql/CompraGraphQL.java
@@ -0,0 +1,51 @@
+package com.udemy.compras.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.udemy.compras.domain.ClienteService;
+import com.udemy.compras.domain.Compra;
+import com.udemy.compras.domain.CompraService;
+import com.udemy.compras.domain.ProdutoService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class CompraGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
+
+    @Autowired
+    private CompraService service;
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    public Compra compra(Long id) {
+        return service.findById(id);
+    }
+
+    public List<Compra> getCompras() {
+        return service.findAll();
+    }
+
+    public Compra saveCompra(CompraInput input) {
+        ModelMapper m = new ModelMapper();
+        Compra c = m.map(input,Compra.class);
+
+        c.setData(new Date());
+
+        c.setCliente(clienteService.findById(input.getClienteId()));
+        c.setProduto(produtoService.findById(input.getProdutoId()));
+
+        return service.save(c);
+    }
+
+    public Boolean deleteCompra(Long id) {
+        return service.deleteById(id);
+    }
+}
